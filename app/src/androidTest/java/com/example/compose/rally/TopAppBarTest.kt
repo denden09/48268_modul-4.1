9ac31af0dced47fcd7d67f8471c23ab876626430
zerox <zeroxleashgaming@gmail.com>
2025-02-25 15:09:52 +0700

    stp 3

--- app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -1,27 +1,8 @@
-/*
- * Copyright 2021 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.compose.rally
 
 import androidx.compose.ui.test.assertIsSelected
-import androidx.compose.ui.test.hasContentDescription
-import androidx.compose.ui.test.hasParent
-import androidx.compose.ui.test.hasText
-import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.junit4.createComposeRule
 import com.example.compose.rally.ui.components.RallyTopAppBar
 import org.junit.Rule
 import org.junit.Test
@@ -33,9 +14,10 @@ class TopAppBarTest {
 
     @Test
     fun rallyTopAppBarTest_currentTabSelected() {
+        val allScreens = RallyScreen.values().toList()
         composeTestRule.setContent {
             RallyTopAppBar(
-                allScreens = RallyScreen.entries,
+                allScreens = allScreens,
                 onTabSelected = { },
                 currentScreen = RallyScreen.Accounts
             )
@@ -44,26 +26,5 @@ class TopAppBarTest {
         composeTestRule
             .onNodeWithContentDescription(RallyScreen.Accounts.name)
             .assertIsSelected()
-    }
-
-    @Test
-    fun rallyTopAppBarTest_currentLabelExists() {
-        composeTestRule.setContent {
-            RallyTopAppBar(
-                allScreens = RallyScreen.entries,
-                onTabSelected = { },
-                currentScreen = RallyScreen.Accounts
-            )
-        }
-
-        composeTestRule
-            .onNode(
-                hasText(RallyScreen.Accounts.name.uppercase()) and
-                    hasParent(
-                        hasContentDescription(RallyScreen.Accounts.name)
-                    ),
-                useUnmergedTree = true
-            )
-            .assertExists()
     }
 }
